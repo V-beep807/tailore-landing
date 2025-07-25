ce5af534058a37c8c9401da755185415a0345ae4
vijji'echo <vijjidasari23@fmail.com>
2025-07-25 15:42:21 +0530

    Fix Vite config and move index.html

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import TailoreLandingPage from "./TailoreLandingPage";
+// src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import TailoreLandingPage from './TailoreLandingPage';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <TailoreLandingPage />
   </React.StrictMode>
 );
+
